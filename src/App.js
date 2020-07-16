dafec143d3dcf822800329f3c41cdbd5785f977f
ayush kumar <kayush242@gmail.com>
2020-07-16 15:16:03 +0530

    added form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,119 @@
 import React from "react";
-
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import "./App.css";
-import Form from "./components/Form";
 
-function App() {
-  return (
-    <div className="App">
-      <Form />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        validationSchema={Yup.object().shape({
+          name: Yup.string()
+            .min(3, "must be atleast 3 characters")
+            .max(15, "must be less than 15 charcters")
+            .required("Name is Required"),
+
+          email: Yup.string()
+            .email("Email is invalid")
+            .required("Email is required"),
+          password: Yup.string()
+            .min(6, "Password must be at least 6 characters")
+            // .matches(/[A-Z]{2}\d{2}[A-Z]{2}\d{4}$/i, "invalid Password")
+            .required("Password is required"),
+          confirmPassword: Yup.string()
+            .oneOf([Yup.ref("password"), null], "Passwords must match")
+            .required("Confirm Password is required"),
+        })}
+        onSubmit={(fields) => {
+          console.log(fields);
+          alert("SUCCESS!! :-)\n\n" + JSON.stringify(fields));
+        }}
+        render={({ errors, status, touched }) => (
+          <Form>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <Field
+                name="name"
+                type="text"
+                className={
+                  "form-control" +
+                  (errors.name && touched.name ? " is-invalid" : "")
+                }
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className="invalid-feedback"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <Field
+                name="email"
+                type="text"
+                className={
+                  "form-control" +
+                  (errors.email && touched.email ? " is-invalid" : "")
+                }
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="invalid-feedback"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <Field
+                name="password"
+                type="password"
+                className={
+                  "form-control" +
+                  (errors.password && touched.password ? " is-invalid" : "")
+                }
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="invalid-feedback"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <Field
+                name="confirmPassword"
+                type="password"
+                className={
+                  "form-control" +
+                  (errors.confirmPassword && touched.confirmPassword
+                    ? " is-invalid"
+                    : "")
+                }
+              />
+              <ErrorMessage
+                name="confirmPassword"
+                component="div"
+                className="invalid-feedback"
+              />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary mr-2">
+                Register
+              </button>
+            </div>
+          </Form>
+        )}
+      />
+    );
+  }
 }
 
-export default App;
+export { App };
